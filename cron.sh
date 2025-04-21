8e33b46faeac8a61f0b95a9d97653c512d20aaba
davit-gitnik <davit.nikoghosyan@alteox.com>
2025-04-22 00:30:59 +0400

    Update cron.sh

--- cron.sh
+++ cron.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd /app 
 npm run grab --- --channels=channels.xml 
-perl -pe 's#(?<!thumbor\.alteox\.app/unsafe/)(http[s]?://[^ ?\"'"'"']+\.(jpg|jpeg|png|webp|gif|svg|tif|tiff|apng|avif))\?([^ \"'"'"']+)#https://thumbor.alteox.app/unsafe/\1%3F\3#g' -i guide.xml 
-sed -i -E '/thumbor\.alteox\.app\/unsafe/!s#(http[s]?://[^ ?\"'"'"']+\.(jpg|jpeg|png|webp|gif|svg|tif|tiff|apng|avif))#https://thumbor.alteox.app/unsafe/\1#g' guide.xml 
-perl -pe 's#(?<!thumbor\.alteox\.app/unsafe/)(http[s]?://images\.media-press\.cloud[^ ?\"'"'"']+)\?([^ \"'"'"']+)#https://thumbor.alteox.app/unsafe/\1%3F\2#g' -i guide.xml 
-perl -pe 's#(?<!thumbor\.alteox\.app/unsafe/)(https?://images\.media-press\.cloud[^ \"'"'"']+)#https://thumbor.alteox.app/unsafe/\1#g' -i guide.xml
+#perl -pe 's#(?<!thumbor\.alteox\.app/unsafe/)(http[s]?://[^ ?\"'"'"']+\.(jpg|jpeg|png|webp|gif|svg|tif|tiff|apng|avif))\?([^ \"'"'"']+)#https://thumbor.alteox.app/unsafe/\1%3F\3#g' -i guide.xml 
+#sed -i -E '/thumbor\.alteox\.app\/unsafe/!s#(http[s]?://[^ ?\"'"'"']+\.(jpg|jpeg|png|webp|gif|svg|tif|tiff|apng|avif))#https://thumbor.alteox.app/unsafe/\1#g' guide.xml 
+#perl -pe 's#(?<!thumbor\.alteox\.app/unsafe/)(http[s]?://images\.media-press\.cloud[^ ?\"'"'"']+)\?([^ \"'"'"']+)#https://thumbor.alteox.app/unsafe/\1%3F\2#g' -i guide.xml 
+#perl -pe 's#(?<!thumbor\.alteox\.app/unsafe/)(https?://images\.media-press\.cloud[^ \"'"'"']+)#https://thumbor.alteox.app/unsafe/\1#g' -i guide.xml
 
